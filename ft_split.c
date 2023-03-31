703f912b8395e0109cc6593f2087ec501f689fd7
Shin Kim <shikim@cx2r5s3.42seoul.kr>
2023-03-31 16:10:06 +0900

    a

--- ft_split.c
+++ ft_split.c
@@ -6,89 +6,70 @@
 /*   By: shikim <shikim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 15:01:58 by shikim            #+#    #+#             */
-/*   Updated: 2023/03/29 16:25:42 by shikim           ###   ########.fr       */
+/*   Updated: 2023/03/29 17:07:32 by shikim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-#include <stdio.h>
-
-static size_t	count_str(char const *s, char c)
+char	*ft_strndup(const char *s, size_t n)
 {
-	size_t	count;
 	size_t	i;
+	char	*str;
 
-	count = 0;
 	i = 0;
-	while (s[i] != '\0')
+	str = (char *)malloc(sizeof(char) * (n + 1));
+	if (str == 0)
+		return (0);
+	while (i < n)
 	{
-		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
-			count++;
+		str[i] = s[i];
 		i++;
 	}
-	return (count);
+	str[i] = '\0';
+	return (str);
 }
 
-static void	count_len_str(char const *s, char c, char **splited_strs)
+size_t	ft_wordcount(char const *s, char c)
 {
-	size_t	len_str;
+	size_t	listsize;
 	size_t	i;
 
-	len_str = 0;
 	i = 0;
+	listsize = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
-		{
-			*splited_strs = (char *)malloc(len_str + 1);
-			len_str = 0;
-			*splited_strs[len_str] = '\0';
-			splited_strs++;
-		}
-		else if (s[i] != c)
-			len_str++;
+		if ((i == 0 && s[i] != c) || \
+		(s[i] == c && s[i + 1] != '\0' && s[i + 1] != c))
+			listsize++;
 		i++;
 	}
-}
-
-static void	copy_str(const char *s, char **splited_strs, char c)
-{
-	size_t	i;
-	size_t	j;
-	size_t	k;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	while (splited_strs[j] != 0)
-	{
-		while (splited_strs[j][k] != '\0')
-		{
-			if (s[i] != c)
-			{
-				splited_strs[j][k] = s[i];
-				printf("s[i]: %c\n", splited_strs[j][k]);
-				k++;
-			}
-			i++;
-		}
-		j++;
-	}
+	return (listsize);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**splited_strs;
-	size_t	num_str;
+	char	**strlist;
+	size_t	i;
+	size_t	k;
+	size_t	save;
 
-	num_str = count_str(s, c);
-	splited_strs = (char **)malloc(sizeof(char *) * (num_str + 1));
-	splited_strs[num_str] = 0;
-	if (splited_strs == 0)
+	i = 0;
+	k = 0;
+	strlist = (char **)malloc(sizeof(char *) * (ft_wordcount(s, c) + 1));
+	if (strlist == 0)
 		return (0);
-	count_len_str(s, c, splited_strs);
-	copy_str(s, splited_strs, c);
-	return (splited_strs);
+	while (i < ft_wordcount(s, c) && s[k] != '\0')
+	{
+		while (s[k] == c)
+			k++;
+		save = k;
+		while (s[k] != c && s[k] != '\0')
+			k++;
+		strlist[i] = ft_strndup(&s[save], k - save);
+		i++;
+	}
+	strlist[i] = 0;
+	return (strlist);
 }
