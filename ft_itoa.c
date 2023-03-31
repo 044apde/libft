bb45f84391f2486226cc78d7431534b097b31b73
044apde <044apde@gmail.com>
2023-04-01 01:21:38 +0900

    ADD ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,3 +11,53 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int check_size(int n)
+{
+	int			size;
+	long long	num;
+
+	size = 0;
+	if (n < 0)
+	{
+		++size;
+		num = (long long)n * -1;
+	}
+	else
+		num = (long long)n;
+	while (num > 0)
+	{
+		++size;
+		num /= 10;
+	}
+	return (size);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*dst;
+	int		dst_size;
+	long long	num;
+
+	dst_size = check_size(n);
+	dst = (char *)malloc(dst_size + 1);
+	dst[dst_size] = '\0';
+	if (n >= 0)
+	{
+		while (dst_size-- > 0)
+		{
+			dst[dst_size] = (num % 10) + 48;
+			num = num / 10; 
+		}
+	}
+	else
+	{
+		while (dst_size-- > 1)
+		{
+			dst[dst_size] = (num % 10) + 48;
+			num = num / 10;
+		}
+		dst[0] = '-';
+	}
+	return (dst);
+}
